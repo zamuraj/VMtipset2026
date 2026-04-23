941135b99d5fd622aa5430e6b16153a1cf2df2d0
zamuraj <124688269+zamuraj@users.noreply.github.com>
2026-04-23 16:46:04 +0200

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// KLISTRA IN DIN CONFIG FRÅN FIREBASE HÄR:
+const firebaseConfig = {
+  apiKey: "AIzaSyDgZeryeAXjnW3u5fExDm029c1IoXLOqTI",
+  authDomain: "kroppsoptimering.firebaseapp.com",
+  projectId: "kroppsoptimering",
+  storageBucket: "kroppsoptimering.firebasestorage.app",
+  messagingSenderId: "676906891678",
+  appId: "1:676906891678:web:89fb6f7411f9062db73e8f",
+  measurementId: "G-LV434RW0CL"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
